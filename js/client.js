3e1eeb010108c2587c890f446e004ce8ee6b0d80
Dustin Shulz <dustin.shulz@edu.sait.ca>
2021-02-19 13:35:00 -0700

    reverted changes for testing

--- js/client.js
+++ js/client.js
@@ -1,7 +1,7 @@
 // Create a constant for the nav element to become visible when EventListener is triggered.
 const nav = document.getElementById('nav');
 // Create a constant for the burger element to become visible when EventListener is triggered.
-const burger = document.querySelector(".burger");
+const burger = document.getElementById('burger');
 // Create a constant for the overlay element to become visible when EventListener is triggered.
 const overlay = document.getElementById('overlay');
 // Create an EventListener associated with the burger icon, when clicked it will activate the three other events.
@@ -10,3 +10,5 @@ burger.addEventListener('click', () => {
     nav.classList.toggle('active');
     overlay.classList.toggle('active');
 });
+
+// document.querySelector
