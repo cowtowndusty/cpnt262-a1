a6b97c1d6b889aa1ed6c28b4319a65962bf02d15
Dustin Shulz <dustin.shulz@edu.sait.ca>
2021-02-19 22:10:55 -0700

    Added label for hamburger menu

--- js/client.js
+++ js/client.js
@@ -1,11 +1,14 @@
 'use strict';
+
+document.getElementById("h-menu").style.display = 'none';
+document.getElementById("h-menu").style.display = 'block';
 // Create a constant for the nav element to become visible when EventListener is triggered.
 const nav = document.querySelector('.nav');
-// Create a constant for the burger element to become visible when EventListener is triggered.
+// Create a constant for the burger element to allow for interactivity when EventListener is triggered.
 const burger = document.querySelector('.burger');
 // Create a constant for the overlay element to become visible when EventListener is triggered.
 const overlay = document.querySelector('.overlay');
-// Create an EventListener associated with the burger icon, when clicked it will activate the three other events.
+// Create an EventListener associated with the burger icon, when clicked it will activate the other events.
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     nav.classList.toggle('active');
